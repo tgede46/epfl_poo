7159d9e2f0847e84b7aa627b5993193810c5680c
tgede46 <gedeonkpara@gmail.com>
2024-12-27 02:21:26 +0000

    mise a la semaine 1

--- POO/semaine1/src/main/java/gedon/net/semaine1/Imc.java
+++ POO/semaine1/src/main/java/gedon/net/semaine1/Imc.java
@@ -1,33 +1,37 @@
 package gedon.net.semaine1;
 
 
+/*******************************************
+ * Completez le programme a partir d'ici.
+ *******************************************/
+
 // * Completez le programme a partir d'ici.
 class Patient {
  //    definir les caracteristiques du patient
- private double masse;
- private double hauteur;
+    private double masse;
+    private double hauteur;
 
  //    definir une methode init
  public void init (double masse, double hauteur) {
- if (masse > 0 && hauteur > 0) {
- this.masse = masse;
- this.hauteur = hauteur;
- } else {
- this.masse = 0;
- this.hauteur = 0;
+     if (masse > 0 && hauteur > 0) {
+        this.masse = masse;
+        this.hauteur = hauteur;
+ }   else {
+        this.masse = 0;
+        this.hauteur = 0;
  }
  }
  //    definir une methode afficher
  public void afficher() {
- System.out.printf("Patient : masse = %.1f kg, hauteur = %.2f m\n", masse, hauteur);
+     System.out.printf("Patient : masse = %.1f kg, hauteur = %.2f m\n", masse, hauteur);
  }
 
  //    definir une methode imc
  public double imc() {
- if (hauteur == 0) {
- return 0;
- } else {
- return masse / (hauteur * hauteur);
+    if (hauteur == 0) {
+        return 0;
+        } else {
+        return masse / (hauteur * hauteur);
  }
  }
  }
