5b7aeb0cc53c2aed1bcafb6977c21df348a109a9
tgede46 <gedeonkpara@gmail.com>
2024-12-27 14:05:01 +0000

    fiel

--- POO/semaine1/src/main/java/gedon/net/semaine2/Labo.java
+++ POO/semaine1/src/main/java/gedon/net/semaine2/Labo.java
@@ -8,79 +8,68 @@ class Souris {
     private int esperanceVie;
     private boolean clonee;
 
-    public Souris(int poids, String couleur,int age,int esperanceVie) {
+    // Constructeur principal
+    public Souris(int poids, String couleur, int age, int esperanceVie) {
         this.poids = poids;
         this.couleur = couleur;
         this.age = age;
-        this.esperanceVie = ESPERANCE_VIE_DEFAUT;
+        this.esperanceVie = esperanceVie;
         System.out.println("Une nouvelle souris !");
     }
 
-    public Souris(Souris souris) {
-        this.poids = poids;
-        this.couleur = couleur;
-        this.age = age;
-        this.esperanceVie = ESPERANCE_VIE_DEFAUT/4;
-        this.clonee = true;
-        System.out.println("le clonage d'une souris !");
-    }
-
+    // Surcharge avec espérance de vie par défaut
     public Souris(int poids, String couleur) {
-        this.poids = poids;
-        this.couleur = couleur;
-        this.age = 0;
-        this.esperanceVie = ESPERANCE_VIE_DEFAUT;
-        System.out.println("Une nouvelle souris !");
+        this(poids, couleur, 0, ESPERANCE_VIE_DEFAUT);
     }
-    public Souris(int poids, String couleur,int age) {
-        this.poids = poids;
-        this.couleur = couleur;
-        this.age = age;
-        this.esperanceVie = ESPERANCE_VIE_DEFAUT;
-        System.out.println("Une nouvelle souris !");
+
+    // Constructeur de clonage
+    public Souris(Souris souris) {
+        this.poids = souris.poids;
+        this.couleur = souris.couleur;
+        this.age = souris.age;
+        this.esperanceVie = (int) (souris.esperanceVie * 0.8); // Réduction de 20%
+        this.clonee = true;
+        System.out.println("Clonage d'une souris !");
     }
 
     @Override
     public String toString() {
-        return "Une souris " + couleur + (clonee ? ", clonee,":"") + " de " + age + " mois et pesant " + poids + " grammes";
+        return "Une souris " + couleur + (clonee ? ", clonee, " : ", ") + "de " + age + " mois et pesant " + poids + " grammes.";
     }
 
     public void vieillir() {
         age++;
-        if (clonee && age > esperanceVie/2) {
-            couleur = "verte";
+        if (clonee && age > esperanceVie / 2) {
+            couleur = "verte"; // Couleur change après la moitié de vie
         }
     }
 
     public void evolue() {
-        for (int i = 0; i < esperanceVie; i++) {
+        while (age < esperanceVie) {
             vieillir();
         }
     }
-
-    /*******************************************
-     * Completez le programme a partir d'ici.
-     *******************************************/
-
 }
 
 /*******************************************
- * Ne rien modifier apres cette ligne.
+ * Ne rien modifier après cette ligne.
  *******************************************/
 
 public class Labo {
 
     public static void main(String[] args) {
-        Souris s1 = new Souris(50, "blanche", 2);
+        Souris s1 = new Souris(50, "blanche", 2, 36);
         Souris s2 = new Souris(45, "grise");
         Souris s3 = new Souris(s2);
 
         System.out.println(s1);
         System.out.println(s2);
         System.out.println(s3);
+
         s1.evolue();
         s2.evolue();
         s3.evolue();
+
         System.out.println(s1);
         System.out.println(s2);
         System.out.println(s3);
